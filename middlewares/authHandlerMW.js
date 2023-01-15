d543df9976e95d40a8a6e44ca154cc2a71f396ee
Ritikg269 <ritikg269@gmail.com>
2023-01-15 12:22:11 +0530

    fixed auth errors

--- middlewares/authHandlerMW.js
+++ middlewares/authHandlerMW.js
@@ -3,18 +3,18 @@ const User = require('../Database/models/user');
 const { authenticationError } = require('./errorHandlerMw');
 const authHandlerMW = async (req, res, next) => {
     try{
-        console.log("hello")
-        console.log(req.session.user)
+        // console.log(req.session.user)
+        // console.log(typeof(req.session.isLoggedIn))
+        console.log()
         if(!req.session.user || !req.session.isLoggedIn)
         throw authenticationError()
-        if(!req.headers['user-id'] || req.headers['is-service-provider'] === null)
+        if(!req.headers['user-id'] || typeof(JSON.parse(req.headers['is-service-provider'])) !=="boolean")
         throw authenticationError()        
-        if(req.session.user.isServiceProvider && req.headers['is-service-provider'] !== 'true')
-        throw authenticationError()        
-        if(!req.session.user.isServiceProvider && req.headers['is-service-provider'] !== 'false')
+        if(req.session.user.isServiceProvider != JSON.parse(req.headers['is-service-provider']))
         throw authenticationError()        
         if(req.session.user._id != req.headers['user-id'] )
-        return next({status: 402, message: "Authentication Error auth 3"}); 
+        throw authenticationError()        
+
         //made a special check if user exist or not in db : todo
         
         
